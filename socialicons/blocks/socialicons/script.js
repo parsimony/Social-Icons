8a223b1db55ad751368c59d7656af4c1185738fb
Parsimony <contact@parsimony-cms.com>
2014-05-28 14:38:53 +0200

    Social-Icons

---
+++ socialicons/blocks/socialicons/script.js
@@ -0,0 +1,20 @@
+function block_socialicons() {
+    
+    block.call(this);  
+    
+    this.name = "socialicons";
+
+    this.stylableElements = {
+	"socialicons":".socialicons",
+	"container":".socialicons ul",
+	"items":".socialicons li",
+	"links":".socialicons a",
+	"images":".socialicons img",
+	}
+}
+
+    
+   
+    
+
+
